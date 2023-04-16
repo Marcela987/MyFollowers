85a5e79a8ad587d8fa648b1413a3b1b3b7d91ea9
Marcela Gonçalves <marcelasouzagoncalves@gmail.com>
2023-04-16 13:20:44 -0300

    Starting components

--- MyFollowers/FavoritesListViewController.swift
+++ MyFollowers/FavoritesListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListViewController.swift
 //  MyFollowers
 //
 //  Created by Marcela Goncalves on 12/04/23.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
     }
-
-
 }
-
---
+++ MyFollowers/SearchViewController.swift
@@ -0,0 +1,61 @@
+//
+//  SearchViewController.swift
+//  MyFollowers
+//
+//  Created by Marcela Goncalves on 12/04/23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    let logoImageView = UIImageView()
+    let usernameTextField = GFTextField()
+    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configureLogoImageView()
+        configureUsernameTextField()
+        configureCallToActionButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "logo")!
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    func configureUsernameTextField() {
+        view.addSubview(usernameTextField)
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 48),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -48),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    func configureCallToActionButton() {
+        view.addSubview(callToActionButton)
+        NSLayoutConstraint.activate([
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -48),
+            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 48),
+            callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -48),
+            callToActionButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+}
